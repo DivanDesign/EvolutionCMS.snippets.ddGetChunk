e5a7e264e2e3e7704ea161733cb7e2842f1a0488
Ilyas Ronef <ilyas@divandesign.biz>
2015-10-06 14:02:06 +0500

    * The “screening” have been renamed as “escaping”. * Be advised! The snippet is now using the “modx.ddTools” 0.12 library!

--- ddGetChunk.php
+++ ddGetChunk.php
@@ -5,10 +5,10 @@
  *
  * Snippet gets the chunk contents by its name. For example, it useful to get chunks in js code.
  * 
- * @uses modx.ddTools lib 0.2.
+ * @uses The library modx.ddTools 0.12.
  *
  * @param name {string: chunkName} - Chunk name. @required
- * @param screening {0; 1} - Screening special chars. Default: 1.
+ * @param escaping {0; 1} - Escaping special chars. Default: 1.
  * @param placeholders {separated string} - Additional data for parsed result chunk. Format: separated string with '::' for pair key-value and '||' between pairs. Default: ''.
  * @param removeEmptyPlaceholders {0; 1} - Placeholders which have not values to be replaced by will be deleted from parsed chunk if the parameter equals 1. Default: 0.
  * 
@@ -18,10 +18,11 @@
  * http://www.DivanDesign.biz
  */
 
+
 if (!empty($name)){
 	//Подключаем modx.ddTools
-	require_once $modx->config['base_path'].'assets/snippets/ddTools/modx.ddtools.class.php';
-
+	require_once $modx->getConfig('base_path').'assets/snippets/ddTools/modx.ddtools.class.php';
+	
 	//Получаем чанк
 	$result = $modx->getChunk($name);
 	
@@ -42,7 +43,7 @@ if (!empty($name)){
 	$result = ddTools::parseSource($result);
 	
 	//Экранируем сиволы, если нужно
-	if (!isset($screening) || $screening == '1'){
+	if (!isset($escaping) || $escaping == '1'){
 		$result = ddTools::screening($result);
 	}
 
