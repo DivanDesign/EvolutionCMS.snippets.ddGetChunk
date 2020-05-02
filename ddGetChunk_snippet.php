74e9e89beee7aac0efdc68705053c6f6cc603a31
Ilyas Ronef <ilyas@DivanDesign.ru>
2020-05-02 12:31:30 +0300

    * Minor changes.

--- ddGetChunk_snippet.php
+++ ddGetChunk_snippet.php
@@ -3,28 +3,31 @@
  * ddGetChunk
  * @version 2.2 (2017-03-01)
  * 
- * @desc Snippet gets the chunk contents by its name. For example, it useful to get chunks inside js code.
+ * @desc Snippet gets the chunk contents by its name. For example, it useful to get chunks inside JS code.
  * 
  * @uses PHP >= 5.4.
  * @uses MODXEvo >= 1.1.
  * @uses MODXEvo.libraries.ddTools >= 0.18.
  * 
- * @param $name {string_chunkName|string} — Chunk name or code via “@CODE:” prefix. @required
- * @param $placeholders {stirng_json|string_queryFormated} — Additional data as JSON (https://en.wikipedia.org/wiki/JSON) or Query string (https://en.wikipedia.org/wiki/Query_string) has to be passed into the chunk. E. g. `{"width": 800, "height": 600}` or `width=800&height=600`. Default: ''.
+ * @param $name {stringChunkName|string} — Chunk name or code via “@CODE:” prefix. @required
+ * @param $placeholders {stirngJsonObject|stringQueryFormated} — Additional data as JSON (https://en.wikipedia.org/wiki/JSON) or Query string (https://en.wikipedia.org/wiki/Query_string) has to be passed into the chunk. E. g. `{"width": 800, "height": 600}` or `width=800&height=600`. Default: ''.
  * @param $removeEmptyPlaceholders {0|1} — Placeholders which have not values to be replaced by will be deleted from parsed chunk if the parameter equals 1. Default: 0.
  * @param $escapeResultForJS {0|1} — Escaping special chars (for js). Default: 0.
  * 
- * @link http://code.divandesign.biz/modx/ddgetchunk/2.2
+ * @link https://code.divandesign.biz/modx/ddgetchunk
  * 
- * @copyright 2009–2017 DivanDesign {@link http://www.DivanDesign.biz }
+ * @copyright 2009–2017 DD Group {@link https://www.DivanDesign.biz }
  */
 
 if (!empty($name)){
-	//Подключаем modx.ddTools
-	require_once($modx->getConfig('base_path').'assets/libs/ddTools/modx.ddtools.class.php');
+	//Include (MODX)EvolutionCMS.libraries.ddTools
+	require_once(
+		$modx->getConfig('base_path') .
+		'assets/libs/ddTools/modx.ddtools.class.php'
+	);
 	
 	//Для обратной совместимости
-	extract(ddTools::verifyRenamedParams(
+	extract(\ddTools::verifyRenamedParams(
 		$params,
 		[
 			'escapeResultForJS' => 'escaping'
@@ -36,28 +39,34 @@ if (!empty($name)){
 	
 	//Если переданы дополнительные данные
 	if (!empty($placeholders)){
-		$placeholders = ddTools::encodedStringToArray($placeholders);
+		$placeholders = \ddTools::encodedStringToArray($placeholders);
 	}else{
 		$placeholders = [];
 	}
 	
 	//Парсим
-	$result = ddTools::parseText([
+	$result = \ddTools::parseText([
 		'text' => $result,
 		'data' => $placeholders,
 		//Удаляем пустые плэйсхолдеры, если нужно
-		'removeEmptyPlaceholders' => isset($removeEmptyPlaceholders) &&	$removeEmptyPlaceholders == '1' ? true : false
+		'removeEmptyPlaceholders' =>
+			(
+				isset($removeEmptyPlaceholders) &&
+				$removeEmptyPlaceholders == '1'
+			) ?
+			true :
+			false
 	]);
 	
 	//Окончательно парсим
-	$result = ddTools::parseSource($result);
+	$result = \ddTools::parseSource($result);
 	
 	//Экранируем сиволы, если нужно
 	if (
 		isset($escapeResultForJS) &&
 		$escapeResultForJS == '1'
 	){
-		$result = ddTools::escapeForJS($result);
+		$result = \ddTools::escapeForJS($result);
 	}
 	
 	return $result;
